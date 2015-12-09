817afb7f53ade583f1d12c30a4849622ebccb3c7
Mirco Veltri <mircoveltri@gmail.com>
2015-12-09 14:55:11 +0100

    using new async routes. beautify file

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 // Required NodeJS modules
 // ------------------------------------------------
 var express = require('express'),
-    path = require('path'),
-    favicon = require('serve-favicon'),
-    logger = require('morgan'),
-    bodyParser = require('body-parser'),
-    config = require('config'),
-    helmet = require('helmet'),
-    cors = require('cors');
+    path        = require('path'),
+    favicon     = require('serve-favicon'),
+    logger      = require('morgan'),
+    bodyParser  = require('body-parser'),
+    config      = require('config'),
+    helmet      = require('helmet'),
+    cors        = require('cors')
+    version     = require('./version');
 
 // Routes setup pages
 // ------------------------------------------------
-var home_page = require('./routes/index'),
-    learn_page = require('./routes/learn'),
-    actions_page = require('./routes/actions'),
+var home_page     = require('./routes/index'),
+    learn_page    = require('./routes/learn'),
+    actions_page  = require('./routes/actions'),
     glossary_page = require('./routes/glossary');
 
 
 // Async Routes setup
 // ------------------------------------------------
-var async_version = require('./routes/async_version'),
-    async_dataset_list = require('./routes/async_dataset_list'),
-    async_create_dataset = require('./routes/async_create_dataset'),
-    async_delete_dataset = require('./routes/async_delete_dataset');
+var async_version             = require('./routes/async/version'),
+    async_dataset_list        = require('./routes/async/dataset_list'),
+    async_create_dataset      = require('./routes/async/create_dataset'),
+    async_delete_dataset      = require('./routes/async/delete_dataset'),
+    async_submit_signals      = require('./routes/async/submit_signals'),
+    async_submit_profiles     = require('./routes/async/submit_profiles'),
+    async_submit_clusters     = require('./routes/async/submit_clusters'),
+    async_submit_predictions  = require('./routes/async/submit_predictions'),
+    async_job_status          = require('./routes/async/job_status');
+
+// Express initialization
+// ------------------------------------------------
 var app = express();
 
 // view engine setup
@@ -30,41 +39,49 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(helmet());
 app.use(cors());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(version);
 
 // App Locals
 // ------------------------------------------------
 app.locals.pretty = true;
 app.locals.neuron_config = {
-  protocol: config.get('protocol'),
   host: config.get('host'),
   port: config.get('port') || 80,
-  neuron_app_id: config.get('neuron_app_id')
+  ssl: config.get('ssl') || false,
+  neuron_app_id: config.get('neuron_app_id'),
+  neuron_app_key: config.get('neuron_app_key')
 };
 app.locals.neuron_header = {
   'Content-Type': 'application/json',
   'Accept': 'application/json',
-  'neuron-application-id': config.neuron_app_id
+  'neuron-application-id': config.neuron_app_id,
+  'neuron-application-key': config.neuron_app_key
 };
 
-// Pages Routes
+// Sync Pages Routes
+// ------------------------------------------------
 app.use('/', home_page);
 app.use('/learn', learn_page);
 app.use('/actions', actions_page);
 app.use('/glossary', glossary_page);
+
 // Async Routes
+// ------------------------------------------------
 app.use('/version', async_version);
 app.use('/dataset_list', async_dataset_list);
 app.use('/create_dataset', async_create_dataset);
 app.use('/delete_dataset', async_delete_dataset);
+app.use('/submit_signals', async_submit_signals);
+app.use('/submit_profiles', async_submit_profiles);
+app.use('/submit_clusters', async_submit_clusters);
+app.use('/submit_predictions', async_submit_predictions);
 
 // Error handlers
 // ------------------------------------------------
