5c7e0ae20e5f67608dc45d505092a236dfb4cfcb
indaco <mircoveltri@gmail.com>
2015-12-10 11:40:22 +0100

    semicolumns added for function definitions

--- utils.js
+++ utils.js
@@ -1,14 +1,14 @@
 buildURL = function(config, path) {
   var protocol = (config.ssl) ? "https" : "http";
   return protocol + "://" + config.host + ":" + config.port + "/1.0" + path;
-}
+};
 
 handleServerError = function(data) {
   return {
     "errorId": data.body.errorId,
     "errorMessage": data.body.errorMessage
   };
-}
+};
 
 exports.buildURL = buildURL;
 exports.handleServerError = handleServerError;
